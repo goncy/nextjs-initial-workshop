ad5fd64ab0829faa489bf49720294c25d287ae0a
goncy <gonzalo.pozzo4@gmail.com>
2021-12-24 16:24:42 -0300

    Initial workshop

--- pages/hot/_middleware.ts
+++
@@ -1,9 +0,0 @@
-import {NextResponse} from "next/server";
-
-import api from "../../api";
-
-export default async function middleware() {
-  const hot = await api.hot();
-
-  return NextResponse.rewrite(`/${hot.id}`);
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,7 @@
-import type {GetStaticProps, NextPage} from "next";
-import type {Store} from "../types";
+import type {NextPage} from "next";
 
-import Link from "next/link";
-
-import api from "../api";
-import StoreCard from "../components/StoreCard";
-
-interface Props {
-  stores: Store[];
-}
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const stores = await api.list();
-
-  return {
-    props: {
-      stores,
-    },
-  };
-};
-
-const Home: NextPage<Props> = ({stores}) => {
-  return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(320px, 1fr))",
-        gap: 12,
-        flexDirection: "column",
-        width: "100%",
-      }}
-    >
-      {stores.map((store) => (
-        <Link key={store.id} href={`/${store.id}`}>
-          <a>
-            <StoreCard store={store} />
-          </a>
-        </Link>
-      ))}
-    </div>
-  );
+const Home: NextPage = () => {
+  return <div>Hello future</div>;
 };
 
 export default Home;
